4b392fcee5d2e15ee49e4ef1aa2b6021f274973a
Abhishek <abhishek.kaushal@dhwaniris.com>
2019-07-03 08:44:57 +0530

    child data added

--- test.py
+++ test.py
@@ -1,35 +1,83 @@
 import face_recognition
 from sklearn import svm
 import os
-import mysql.connector
 import face_recognition
 import numpy as np
 from PIL import Image, ImageDraw
 import uuid
+import pickle
+
+filename = "./finalized_model.sav"
+
+# load the model from disk
+clf = pickle.load(open(filename, 'rb'))
+
+image_path = os.listdir("C:/Users/HP/Desktop/data/")
+
+for file in image_path:
 
 
-test_image = face_recognition.load_image_file("C:/Users/HP/face_recognition_examples/img/Dhwani/photos/12.jpg")
-unknown_face = face_recognition.face_encodings(test_image)
-face_locations = face_recognition.face_locations(test_image)
+	test_image = face_recognition.load_image_file('C:/Users/HP/Desktop/data/'+file)
 
-# Convert to PIL format
-pil_image = Image.fromarray(test_image)
-# Create a ImageDraw instance
-draw = ImageDraw.Draw(pil_image)
+	# Find all the faces in the test image using the default HOG-based model
+	face_locations = face_recognition.face_locations(test_image)
+	no = len(face_locations)
+	#print("Number of faces detected: ", no)
 
-for(top, right, bottom, left), unknown_face in zip(face_locations,unknown_face):
-	#print(unknown_face)
-	#name = clf.predict(unknown_face)
-	name = "unknown"
+	# Convert to PIL format
+	pil_image = Image.fromarray(test_image)
+	# Create a ImageDraw instance
+	draw = ImageDraw.Draw(pil_image)
 
-	draw.rectangle(((left, top), (right, bottom)), outline=(255,255,0))
-	# Draw label
-	text_width, text_height = draw.textsize(name)
-	draw.rectangle(((left,bottom - text_height - 10), (right, bottom)), fill=(255,255,0), outline=(255,255,0))
-	draw.text((left + 6, bottom - text_height - 5), name, fill=(0,0,0))
+	# Predict all the faces in the test image using the trained classifier
+	print("Found: \n")
+	for i in range(no):
+		top, right, bottom, left = face_locations[i]
+		test_image_enc = face_recognition.face_encodings(test_image)[i]
+		name = clf.predict([test_image_enc])
+		naam=str(name)
+		draw.rectangle(((left, top), (right, bottom)), outline=(255,255,0))
+		# Draw label
+		text_width, text_height = draw.textsize(naam)
+		draw.rectangle(((left,bottom - text_height - 10), (right, bottom)), fill=(255,255,0), outline=(255,255,0))
+		draw.text((left + 6, bottom - text_height - 5),naam, fill=(0,0,0))
 
-del draw
-# Save image
-file = "./detected/" + str(uuid.uuid1()) + ".jpg"
-pil_image.save(file)
+	del draw
+	# Save image
+	file = "./detected/" + str(uuid.uuid1()) + ".jpg"
+	pil_image.save(file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
